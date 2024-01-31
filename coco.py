99122cfb6fb6e35dd83499891e668c8743b9da07
ivansaul <saul_2503@hotmail.com>
2024-01-31 18:33:37 -0500

    feat(cli): updete download command
    - implement download course by url
    - add headless mode option

--- coco.py
+++ coco.py
@@ -7,11 +7,14 @@ from typing import Annotated
 
 import typer
 from rich import print as tprint
+from rich.console import Console
+from rich.table import Table
 
 from facilito import consts, helpers  # type: ignore
 from facilito.core import Client  # type: ignore
-from facilito.errors import DownloadError, VideoError  # type: ignore
+from facilito.errors import CourseError, DownloadError, VideoError  # type: ignore
 from facilito.models.video import Quality  # type: ignore
+from facilito.utils.logger import cli_logger  # type: ignore
 
 app = typer.Typer(
     rich_markup_mode="markdown",
@@ -30,6 +33,7 @@ def download(
         default=Quality.BEST.value,
         prompt_required=True,
     ),
+    headless: bool = False,
 ):
     """
     Downloads [VIDEO|COURSE] from the provided URL.
@@ -47,7 +51,7 @@ def download(
         raise typer.Exit()
 
     if helpers.is_video_url(url):
-        with Client() as client:
+        with Client(headless=headless) as client:
             tprint("⠹ Processing...")
             try:
                 video = client.video(url)
@@ -76,7 +80,68 @@ def download(
         raise typer.Exit()
 
     if helpers.is_course_url(url):
-        pass
+        with Client(headless=headless) as client:
+            tprint("⠹ Processing...")
+
+            try:
+                course = client.course(url)
+                course_title = helpers.clean_string(course.title)
+                course_sections = course.sections
+                # Course Details Table
+                console = Console()
+                title_table = Table(course_title)
+                console.print(title_table)
+                sections_table = Table("Sections", "Videos")
+                for section in course_sections:
+                    sections_table.add_row(
+                        section.title,
+                        str(len(section.videos_url)),
+                    )
+                console.print(sections_table)
+            except CourseError as e:
+                tprint("✗ Unable to download the course.")
+                raise typer.Exit() from e
+
+            # Confirm download
+            confirm_download = typer.confirm("Would you like to download?")
+            if not confirm_download:
+                raise typer.Exit()
+
+            # iterate over sections
+            course_sections = course.sections
+            for section in course_sections:
+                section_title = helpers.clean_string(section.title)
+                section_videos = section.videos_url
+                for video_url in section_videos:
+                    try:
+                        video = client.video(video_url)
+                    except VideoError as e:
+                        tprint("✗ Unable to fetch the video details.")
+                        message = f"[SECTION] {section_title} [VIDEO] {video_url}"
+                        cli_logger.error(message)
+                        continue
+                    max_retries = 5
+                    for attempt in range(1, max_retries + 1):
+                        try:
+                            tprint("⠹ Downloading...")
+                            tprint("⠹", video.title, " ...")
+                            client.refresh_cookies()
+                            # Download directory path
+                            dir_path = (
+                                f"{consts.DOWNLOADS_DIR}/{course_title}/{section_title}"
+                            )
+                            video.download(quality=quality, dir_path=dir_path)
+                        except DownloadError:
+                            if attempt < max_retries:
+                                tprint("⠹ An error occurred while downloading :(")
+                                tprint("⠹ Retrying ...")
+                            else:
+                                tprint("✗ Unable to download the video.")
+                                break
+                        else:
+                            tprint("✓ Done!")
+
+        raise typer.Exit()
 
     tprint("[bold red]Error![/bold red] Thats not a valid [VIDEO|COURSE] URL")
     raise typer.Exit()
