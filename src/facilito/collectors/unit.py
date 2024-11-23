28a02842c58ab945764f56ce985422034200dbc7
ivansaul <saul_2503@hotmail.com>
2024-11-23 10:09:29 -0500

    feat: add support for quiz downloads

--- src/facilito/collectors/unit.py
+++ src/facilito/collectors/unit.py
@@ -2,7 +2,7 @@ from playwright.async_api import BrowserContext
 
 from ..errors import UnitError
 from ..helpers import slugify
-from ..models import Unit
+from ..models import TypeUnit, Unit
 from ..utils import get_unit_type
 
 
@@ -10,6 +10,17 @@ async def fetch_unit(context: BrowserContext, url: str):
     NAME_SELECTOR = ".title-section header h1"
 
     try:
+        type = get_unit_type(url)
+
+        if type == TypeUnit.QUIZ:
+            # TODO: implement quiz fetching
+            return Unit(
+                type=type,
+                url=url,
+                name="quiz",
+                slug="quiz",
+            )
+
         page = await context.new_page()
         await page.goto(url)
 
@@ -17,8 +28,6 @@ async def fetch_unit(context: BrowserContext, url: str):
 
         if not name:
             raise UnitError()
-
-        type = get_unit_type(url)
 
     except Exception:
         raise UnitError()
